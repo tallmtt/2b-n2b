390d72bff6bab9ec365199c339c84487b0fcf32e
Matthew Rensberry <tallmtt@gmail.com>
2022-04-04 13:43:13 -0400

    init

---
+++ sw.js
@@ -0,0 +1,31 @@
+// (A) FILES TO CACHE
+const cName = "demo-pwa",
+cFiles = [
+  "index.html",
+  "style.css",
+  "main.js"
+];
+
+// (B) CREATE/INSTALL CACHE
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(cName)
+    .then((cache) => { return cache.addAll(cFiles); })
+    .catch((err) => { console.error(err) })
+  );
+});
+
+// (C) CACHE STRATEGY
+self.addEventListener("fetch", (evt) => {
+  // (C1) LOAD FROM CACHE FIRST, FALLBACK TO NETWORK IF NOT FOUND
+  event.respondWith(
+    caches.match(evt.request)
+    .then((res) => { return res || fetch(evt.request); })
+  );
+
+  /* (C2) LOAD WITH NETWORK FIRST, FALLBACK TO CACHE IF OFFLINE
+  evt.respondWith(
+    fetch(evt.request)
+    .catch(() => { return caches.match(evt.request); })
+  );*/
+});
